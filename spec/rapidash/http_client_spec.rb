dad58c5fc049d08e1f867fd5d5bebd6822487e37
Paul Springett <paul@springett.me>
2013-05-28 16:14:18 +0100

    Remove specs that call Rapidash::Resonse (now unused)

--- spec/rapidash/http_client_spec.rb
+++ spec/rapidash/http_client_spec.rb
@@ -50,7 +50,6 @@ describe Rapidash::HTTPClient do
       let!(:subject) { HTTPTester.new(options) }
 
       it "should authorize with login and password" do
-        subject.should_receive(:process_response).with("response", :get, {})
         subject.connection.should_receive(:basic_auth).with(options[:login], options[:password])
         subject.connection.stub_chain('app.call').and_return("response")
         subject.request(:get, "foo")
@@ -58,52 +57,8 @@ describe Rapidash::HTTPClient do
     end
 
     it "should call response" do
-        subject.should_receive(:process_response).with("response", :get, {})
       subject.connection.should_receive(:run_request).with(:get, "http://example.com/foo", nil, nil).and_return("response")
-        subject.request(:get, "foo")
-    end
-  end
-
-  describe ".process_response" do
-
-    let!(:valid_response) { OpenStruct.new(:status => "200")}
-    let!(:redirect_response) { OpenStruct.new(:status => "301", :headers => {"location" => "http://example.com/redirect"})}
-    let!(:error_response) { OpenStruct.new(:status => "404")}
-
-    before(:each) do
-      subject.site = "http://example.com"
-    end
-
-    describe "valid response" do
-      before(:each) do
-        Rapidash::Response.should_receive(:new).and_return("response")
-      end
-        
-      it "should return a response object" do
-        response = subject.process_response(valid_response, :get, {})
-        response.should eql("response")
-      end
-
-      it "should perform a redirect" do
-        subject.should_receive(:request).with(:get, "http://example.com/redirect", anything).and_return(subject.process_response(valid_response, :get, {}))
-        response = subject.process_response(redirect_response, :get, {})
-        response.should eql("response")
-      end
-    end
-
-    describe "error response" do
-      it "should not raise an error by default" do
-        response = subject.process_response(error_response, :get, {})
-        response.should be_nil
-      end
-
-      it "should raise an error if the option is set" do
-          subject = HTTPErrorTester.new
-          expect {
-            response = subject.process_response(error_response, :get, {})
-          }.to raise_error(Rapidash::ResponseError)
-      end
-
+      subject.request(:get, "foo")
     end
   end
 end
