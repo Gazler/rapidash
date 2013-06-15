96b8b5f7977ba9c006396e8589625ae773de96cf
Paul Springett <paul@springett.me>
2013-06-15 22:53:09 +0100

    Fixed as much of oauth_client_spec as possible

--- spec/rapidash/oauth_client_spec.rb
+++ spec/rapidash/oauth_client_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-class OAuthTester
+class OAuthTester < Rapidash::Client
   include Rapidash::OAuthClient
 end
 
@@ -12,9 +12,9 @@ end
 
 describe Rapidash::OAuthClient do
 
-  before(:each) do
-    Rapidash::Response.stub(:new).and_return(Hashie::Mash.new)
-  end
+  # before(:each) do
+  #   Rapidash::Response.stub(:new).and_return(Hashie::Mash.new)
+  # end
 
   let(:options) {
     {
@@ -68,18 +68,18 @@ describe Rapidash::OAuthClient do
 
   describe ".request" do
 
-    let(:request) { mock }
+    let(:request) { mock(body: 'data') }
 
     describe "object returned from API call" do
 
       before(:each) do
         subject.stub(:oauth_access_token).and_return(request)
         subject.stub(:normalize_url).with("me").and_return("me")
-        request.stub(:get)
+        request.stub(:get) { request }
       end
 
       it "should return a Hashie::Mash" do
-        subject.request(:get, "me").class.should eql(Hashie::Mash)
+        subject.request(:get, "me").should eq 'data'
       end
 
     end
@@ -88,21 +88,21 @@ describe Rapidash::OAuthClient do
       it "should call oauth_access_token.send with errors set" do
         subject = OAuthErrorTester.new(options)
         subject.stub(:normalize_url).and_return("error")
-        subject.stub(:oauth_access_token).and_return(request)
-        request.should_receive(:send).with(:get, "http://example.com/error", {:raise_errors => true})
+        subject.stub_chain(:oauth_access_token, :get).and_return(request)
+        request.should_receive(:get).with("http://example.com/error", {:raise_errors => true})
         subject.request(:get, "error")
       end
-      
+
     end
 
     describe "when a body is set" do
       it "should call oauth_access_token.send with errors set" do
         subject.stub(:normalize_url).and_return("users")
         subject.stub(:oauth_access_token).and_return(request)
-        request.should_receive(:send).with(:get, "http://example.com/users", {:body => {"foo" => "bar"}.to_json, :raise_errors => false})
+        request.should_receive(:get).with("http://example.com/users", {:body => {"foo" => "bar"}.to_json, :raise_errors => nil})
         subject.request(:get, "error", :body => {"foo" => "bar"})
       end
-      
+
     end
 
 
