07deab267f5082fc3c153bce8bede7f409959ab0
Paul Springett <paul@springett.me>
2013-06-27 13:34:12 +0100

    Fix mattr_accessor require issue

--- lib/rapidash.rb
+++ lib/rapidash.rb
@@ -1,5 +1,6 @@
 #Required for pluralization and camelcasing
 require "active_support/core_ext/string"
+require "active_support/core_ext/module/attribute_accessors"
 require "json"
 
 require "rapidash/version"
@@ -18,15 +19,7 @@ require "rapidash/oauth_client"
 require "rapidash/test_client"
 
 module Rapidash
-  def self.response_exception_class=(obj)
-    @@response_exception_class = obj
-  end
-
-  def self.response_exception_class
-    @@response_exception_class = nil unless defined? @@response_exception_class
-
-    @@response_exception_class
-  end
+  mattr_accessor :response_exception_class
 
   class ParseError < StandardError; end
   class ConfigurationError < StandardError; end
